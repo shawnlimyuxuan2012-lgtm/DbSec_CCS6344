db438ef80268e25eeafba829cc53f925e658c2a3
shawnlimyuxuan2012-lgtm <shawnlimyuxuan2012@gmail.com>
2026-05-21 20:48:04 +0800

    Enhance RBAC.sql for user creation and permissions

--- RBAC.sql
+++ RBAC.sql
@@ -1,23 +1,44 @@
 USE [master];
 GO
 
--- Create Login for the Application Layer
-CREATE LOGIN WebAppUser WITH PASSWORD = 'StrongPassword2026!', CHECK_POLICY = ON;
+IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'WebAppUser')
+BEGIN
+    CREATE LOGIN WebAppUser WITH PASSWORD = 'StrongPassword2026!', CHECK_POLICY = ON;
+END
 GO
 
+-- FIXED: Added the keyword 'SECURITY' for SQL Server 2022 compliance
+GRANT VIEW SERVER SECURITY AUDIT TO WebAppUser;
+GO
+    
 USE SecureStudentDB;
 GO
 
--- Create Database User mapped to Login
-CREATE USER WebAppUser FOR LOGIN WebAppUser;
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'WebAppUser')
+BEGIN
+    CREATE USER WebAppUser FOR LOGIN WebAppUser;
+END
 GO
 
--- Assign Permissions (Least Privilege)
-GRANT SELECT, INSERT ON dbo.Users TO WebAppUser;
-GRANT SELECT, INSERT ON dbo.Submissions TO WebAppUser;
-GRANT SELECT ON dbo.Assessments TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.users TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.submissions TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.assignments TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.grades TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.classes TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.class_students TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.exams TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.exam_submissions TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.exam_questions TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.exam_answers TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.password_reset_tokens TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.audit_logs TO WebAppUser;
+GRANT SELECT, INSERT, UPDATE ON dbo.grade_access_logs TO WebAppUser;
+GO
 
--- Explicitly DENY dangerous actions to prevent Internal Attacks
-DENY DELETE ON dbo.Users TO WebAppUser;
-DENY DELETE ON dbo.Submissions TO WebAppUser;
+DENY DELETE ON dbo.users TO WebAppUser;
+DENY DELETE ON dbo.submissions TO WebAppUser;
+DENY DELETE ON dbo.assignments TO WebAppUser;
+DENY DELETE ON dbo.grades TO WebAppUser;
+DENY DELETE ON dbo.classes TO WebAppUser;
+DENY DELETE ON dbo.exams TO WebAppUser;
 GO
