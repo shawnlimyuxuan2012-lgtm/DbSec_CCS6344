feeb8b7a7a0ff8cd6395faf26ebc879562fd1f6f
shawnlimyuxuan2012-lgtm <shawnlimyuxuan2012@gmail.com>
2026-05-21 20:52:42 +0800

    Update audit configuration for server and database

--- audit_config.sql
+++ audit_config.sql
@@ -1,21 +1,25 @@
 USE [master];
 GO
 
--- 1. Create Server-Level Audit
-CREATE SERVER AUDIT StudentDataAudit
-TO FILE ( FILEPATH = 'C:\SQLAudits\' ); 
+IF NOT EXISTS (SELECT * FROM sys.server_audits WHERE name = 'StudentServerAudit')
+BEGIN
+    CREATE SERVER AUDIT StudentServerAudit
+    TO FILE ( FILEPATH = 'C:\SQLAudits\' ,MAXSIZE = 10 MB )
+    WITH ( QUEUE_DELAY = 1000, ON_FAILURE = CONTINUE );
+END
 GO
 
--- 2. Enable the Audit
-ALTER SERVER AUDIT StudentDataAudit WITH (STATE = ON);
+ALTER SERVER AUDIT StudentServerAudit WITH (STATE = ON);
 GO
 
 USE SecureStudentDB;
 GO
 
--- 3. Define what to record (DML actions on the Student Schema)
-CREATE DATABASE AUDIT SPECIFICATION [Audit_Student_Access]
-FOR SERVER AUDIT StudentDataAudit
-ADD (SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[dbo] BY [public])
-WITH (STATE = ON);
+IF NOT EXISTS (SELECT * FROM sys.database_audit_specifications WHERE name = 'StudentDBAuditSpec')
+BEGIN
+    CREATE DATABASE AUDIT SPECIFICATION StudentDBAuditSpec
+    FOR SERVER AUDIT StudentServerAudit
+    ADD (SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[dbo] BY [public])
+    WITH (STATE = ON);
+END
 GO
